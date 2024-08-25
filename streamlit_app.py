4cb18c093fddfb670ef41f5fed9308de165ec647
chasejcav <chasejcav@gmail.com>
2024-08-25 15:30:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ from matplotlib.colors import LinearSegmentedColormap
 import numpy as np
 import plotly.graph_objects as go
 import requests
+from datetime import timedelta
 
 
 # Function to fetch adjusted stock prices
